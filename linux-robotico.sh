18df8b7562f38d49936a1c49f31ba185f913ca11
Thalles Campagnani <thallescampagnani@gmail.com>
2022-05-07 23:40:00 +0000

    Mudando temporariamente o repositório para ros2-testing
    O pacote ros-humble-desktop e suas dependências ainda não se encontram no repositório ros2, ocasionando erro no script.

--- linux-robotico.sh
+++ linux-robotico.sh
@@ -79,7 +79,7 @@ echo "---> Executando passo a passo fornecido pelo desenvolvedor <---"
 echo ""
 sudo apt install gnupg lsb-release
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2-testing/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
 sudo apt install ros-humble-desktop -y
 source /opt/ros/humble/setup.bash
