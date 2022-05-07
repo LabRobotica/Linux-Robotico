a96de9911c1ac867d30bd77e226e1e3ebaa9fe5a
Thalles Campagnani <thallescampagnani@gmail.com>
2022-05-07 23:57:09 +0000

    Adicionando Visual Studio Code

--- linux-robotico.sh
+++ linux-robotico.sh
@@ -54,6 +54,26 @@ sudo apt install git chromium-browser deepin-icon-theme qt5-gtk2-platformtheme g
 clear
 echo "####################################################################################"
 echo "##										##"
+echo "##                            Instalando VsCode 					##"
+echo "##										##"
+echo "####################################################################################"
+echo ""
+echo "---> Executando passo a passo fornecido pelo desenvolvedor <---"
+echo ""
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code # or code-insiders
+
+
+
+clear
+echo "####################################################################################"
+echo "##										##"
 echo "##                            Instalando Spotify					##"
 echo "##										##"
 echo "####################################################################################"
